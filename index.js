fea50d5ff2c759ff2213e22565eb8b88facf8d11
Shaun Wakashige <theswak@gmail.com>
2019-03-12 08:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(days, holiday) {
+  return `It/'s ${days} days until ${holiday}`
+}
